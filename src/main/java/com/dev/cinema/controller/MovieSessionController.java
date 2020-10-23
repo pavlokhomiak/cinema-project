3bb5f410dd0bd1c68bab0cbb7b76bddba036741f
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-10-23 14:02:50 +0300

    remove formatter

--- src/main/java/com/dev/cinema/controller/MovieSessionController.java
+++ src/main/java/com/dev/cinema/controller/MovieSessionController.java
@@ -4,6 +4,7 @@ import com.dev.cinema.model.dto.MovieSessionRequestDto;
 import com.dev.cinema.model.dto.MovieSessionResponseDto;
 import com.dev.cinema.service.MovieSessionService;
 import com.dev.cinema.util.mapper.MovieSessionMapper;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,10 +34,8 @@ public class MovieSessionController {
     @GetMapping("/available")
     public List<MovieSessionResponseDto> getMovieSessionDto(
             @RequestParam(name = "movieId") Long id,
-            @RequestParam(name = "date") String date) {
-        LocalDate localDate = LocalDate.parse(date,
-                DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return movieSessionService.findAvailableSessions(id, localDate).stream()
+            @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate date) {
+        return movieSessionService.findAvailableSessions(id, date).stream()
                 .map(movieSessionMapper::toMovieSessionResponseDto)
                 .collect(Collectors.toList());
     }
